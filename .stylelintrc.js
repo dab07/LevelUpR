b632b338cf3268650fda8378d68f58cf662254f4
dab07 <harshitsh0711@gmail.com>
2025-09-10 01:19:40 +0530

    [NETLIFY] build fix 2

--- .stylelintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['stylelint-config-standard'],
-  ignoreFiles: ['dist/**/*'],
-  rules: {
-    'block-no-empty': null, // Allow empty blocks in generated files
-  },
-};
